d7e533a84bce18e662ef2f54fdde982fb4b25ee6
Davide <davidepalladino@hotmail.com>
2020-05-03 11:43:20 +0200

    Update to 1.1.1

--- Button.cpp
+++ Button.cpp
@@ -1,27 +1,30 @@
 #include "Button.h"
 
-Button::Button(unsigned short pin) {
+Button::Button(uint8_t pin) : Button(pin, NO_PULLUP, DEFAULT_LONG_PRESSURE, NULL, NULL) {}
+
+Button::Button(uint8_t pin, input mode) : Button(pin, mode, DEFAULT_LONG_PRESSURE, NULL, NULL) {}
+
+Button::Button(uint8_t pin, uint32_t timeLongPress) : Button(pin, NO_PULLUP, timeLongPress, NULL, NULL) {}
+
+Button::Button(uint8_t pin, input mode, uint32_t timeLongPress) : Button(pin, mode, timeLongPress, NULL, NULL) {}
+
+Button::Button(uint8_t pin, uint32_t timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong) : Button(pin, NO_PULLUP, timeLongPress, ptrActionShort, ptrActionLong) {}
+
+Button::Button(uint8_t pin, input mode, uint32_t timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong) {
     setPin(pin);
-    setTimeLongPress(DEFAULT_LONG_PRESSURE);
-    setPtrActionLong(NULL);
-    setPtrActionShort(NULL);
-}
+    setMode(mode);
 
-Button::Button(unsigned short pin, unsigned long timeLongPress) : Button (pin) {
+    setValuePress();
+
+    pinMode(getPin(), getMode());
+    
     setTimeLongPress(timeLongPress);
-}
 
-Button::Button(unsigned short pin, unsigned long timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong) : Button(pin, timeLongPress) {
-    setPtrActionLong(ptrActionLong);
     setPtrActionShort(ptrActionShort);
+    setPtrActionLong(ptrActionLong);
 }
 
-void Button::setPin(unsigned short pin) {
-    this->pin = pin;
-    pinMode(getPin(), INPUT_PULLUP);
-}
-
-void Button::setTimeLongPress(unsigned long timeLongPress) {
+void Button::setTimeLongPress(uint32_t timeLongPress) {
     this->timeLongPress = timeLongPress;
 }
 
@@ -33,25 +36,25 @@ void Button::setPtrActionLong(ptrProcedure ptrActionLong) {
     this->ptrActionLong = ptrActionLong;
 }
 
-unsigned short Button::getPin() {
-    return this->pin;
-}
-
-unsigned long Button::getTimeLongPress() {
+uint32_t Button::getTimeLongPress() {
     return this->timeLongPress;
 }
 
-unsigned short Button::checkPress() {    
+uint8_t Button::checkPress() {   
     /* Checking if is the first press. */
-    if (digitalRead(getPin()) == HIGH && !getPressed()) {
-        setPressed(true);
-        setTimeOut(millis() + getTimeLongPress());
+    if (digitalRead(getPin()) == getValuePress() && !getStatus()) {
+        setStatus(true);
+
+        /* Checking if is set the time of long press. If there is a value, will be set the timeout. */
+        if (getTimeLongPress() > 0) {
+            setTimeOut(millis() + getTimeLongPress());
+        }
 
     /* Checking if is the long press. */
-    } else if ((digitalRead(getPin()) == HIGH) && getPressed() && (millis() >= getTimeOut())) {
-        setValuePress(-1);
-        setPressed(false);
-        
+    } else if ((digitalRead(getPin()) == getValuePress()) && getStatus() && (millis() >= getTimeOut()) && (getTimeLongPress() > 0)) {
+        setActualValue(-1);
+        setStatus(false);
+
         /* If there is a pointer to a procedure, will be executed. */
         if (this->ptrActionLong != NULL) {
            this->ptrActionLong();
@@ -59,10 +62,10 @@ unsigned short Button::checkPress() {
            while (digitalRead(getPin()) == HIGH); 
         }
 
-    /* Checking if is the short press. */      
-    } else if ((digitalRead(getPin()) == LOW) && getPressed() && (millis() < getTimeOut()) && (getValuePress() != -1)) {
-        setValuePress(1);
-        setPressed(false);
+    /* Checking if is the short press, verifying if is set the "timeLongPress" or not. */      
+    } else if ((digitalRead(getPin()) == !getValuePress()) && getStatus() && (((getActualValue() != -1) && (millis() < getTimeOut())) || (getTimeLongPress() == 0))) {
+        setActualValue(1);
+        setStatus(false);
 
         /* If there is a pointer to a procedure, will be executed. */
         if (this->ptrActionShort != NULL) {
@@ -70,34 +73,68 @@ unsigned short Button::checkPress() {
         }
 
     /* Checking if there is not a press. */
-    } else if (digitalRead(getPin()) == LOW) {
-        setValuePress(0);
-        setPressed(false);
+    } else if (digitalRead(getPin()) == !getValuePress()) {
+        setActualValue(0);
+        setStatus(false);
     }
 
-    return getValuePress();
+    return getActualValue();
 }
 
-void Button::setTimeOut(unsigned long timeOut) {
+void Button::setPin(uint8_t pin) {
+    this->pin = pin;
+}
+
+void Button::setMode(input mode) {
+    /* Translation of "mode" parameter "NO_PULLUP"/"PULLUP" to the rispective "INPUT"/"INPUT_PULLUP". */
+    if (mode == NO_PULLUP) {
+        this->mode = INPUT;
+    } else if (mode == PULLUP) {
+        this->mode = INPUT_PULLUP;
+    }
+}
+
+void Button::setValuePress() {
+    /* Checking what is the value of press, "HIGH" if the mode is INPUT; "LOW" if is INPUT_PULLUP. */
+    if (getMode() == INPUT) {
+        valuePress = HIGH;
+    } else if (getMode() == INPUT_PULLUP) {
+        valuePress = LOW;
+    }
+}
+
+void Button::setTimeOut(uint32_t timeOut) {
     this->timeOut = timeOut;
 }
 
-void Button::setPressed(boolean pressed) {
-    this->pressed = pressed;
+void Button::setStatus(boolean status) {
+    this->status = status;
 }
 
-void Button::setValuePress(short valuePress) {
-    this->valuePress = valuePress;
+void Button::setActualValue(int8_t actualValue) {
+    this->actualValue = actualValue;
 }
 
-unsigned long Button::getTimeOut() {
+uint8_t Button::getPin() {
+    return this->pin;
+}
+
+uint8_t Button::getMode() {
+    return this->mode;
+}
+
+uint8_t Button::getValuePress() {
+    return this->valuePress;
+}
+
+uint32_t Button::getTimeOut() {
     return this->timeOut;
 }
 
-boolean Button::getPressed() {
-    return this->pressed;
+boolean Button::getStatus() {
+    return this->status;
 }
 
-short Button::getValuePress() {
-    return this->valuePress;
+int8_t Button::getActualValue() {
+    return this->actualValue;
 }
--- Button.h
+++ Button.h
@@ -6,7 +6,7 @@
   * 
   * @author Davide Palladino
   * @contact davidepalladino@hotmail.com
-  * @version 1.0
+  * @version 1.1.1
   * @date April 20th, 2020
   * 
   * This library is free software; you can redistribute it and/or
@@ -28,7 +28,9 @@
         #include <Arduino.h>                                // Import the Arduino library.
     #endif
 
-    #define DEFAULT_LONG_PRESSURE 5000                      // Default value in milliseconds for the long press.
+    #define DEFAULT_LONG_PRESSURE 0                         // Default value in milliseconds for the long press.
+
+    typedef enum {PULLUP, NO_PULLUP} input;                 // Symbolic costants to indicate, respectively, if is "INPUT_PULLUP" or "INPUT".
 
     /**
      *  Poiter type to a procdeure, that will be assigned to the member data "ptrActionShort" and "ptrActionLong" through the costructor or
@@ -43,38 +45,60 @@
     class Button {
         public:            
             /** 
-             * This constructor creates the object setting only the pin button. 
+             * This constructor creates the object setting only the pin button. Moreover, it calls "pinMode".
              * @param pin Digital pin of the button.
+             * CAUTION: The pin will be set to "NO_PULLUP" mode, respectively "INPUT" of "pinMode".
              */
-            Button(unsigned short pin);
+            Button(uint8_t pin);
 
             /** 
-             * This constructor creates the object setting the pin button and the time to define the long press.
+             * This constructor creates the object setting the pin button and the mode of the input. Moreover, it calls "pinMode".
+             * @param pin Digital pin of the button.
+             * @param mode Mode of the input, between "INPUT" (with "NO_PULLUP" constant) and "INPUT_PULLUP" (with "PULLUP" constant).
+             */
+            Button(uint8_t pin, input mode);
+
+            /** 
+             * This constructor creates the object setting the pin button and the time to define the long press. Moreover, it calls "pinMode".
              * @param pin Digital pin of the button.
              * @param timeLongPress Time in milliseconds for long press.
+             * CAUTION: The pin will be set to "NO_PULLUP" mode, respectively "INPUT" of "pinMode".
              */
-            Button(unsigned short pin, unsigned long timeLongPress);
+            Button(uint8_t pin, uint32_t timeLongPress);
 
             /** 
-             * This constructor creates the object setting the pin button and the time to define the long press.
+             * This constructor creates the object setting the pin button, the mode of the input and the time to define the long press. Moreover, it calls "pinMode".
+             * @param pin Digital pin of the button.
+             * @param mode Mode of the input, between "INPUT" (with "NO_PULLUP" constant) and "INPUT_PULLUP" (with "PULLUP" constant).
+             * @param timeLongPress Time in milliseconds for long press.
+             */
+            Button(uint8_t pin, input mode, uint32_t timeLongPress);
+
+            /** 
+             * This constructor creates the object setting the pin button, the time to define the long press and the procedures for short and long press. Moreover, it calls "pinMode".
              * @param pin Digital pin of the button.
              * @param timeLongPress Time in milliseconds for long press.
              * @param ptrActionShort Pointer to a procedure for the short press. The procedure must be without parameters.
              * @param ptrActionLong Pointer to a procedure for the long press. The procedure must be without parameters.
+             * CAUTION: The pin will be set to "NO_PULLUP" mode, respectively "INPUT" of "pinMode".
              */
-            Button(unsigned short pin, unsigned long timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong);
-            
-            /**
-             * This method sets the pin button.
+            Button(uint8_t pin, uint32_t timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong);
+
+            /** 
+             * This constructor creates the object setting the pin button, the mode of the input, the time to define the long press and the procedures for short and long press. Moreover, it calls "pinMode".
              * @param pin Digital pin of the button.
+             * @param mode Mode of the input, between "INPUT" (with "NO_PULLUP" constant) and "INPUT_PULLUP" (with "PULLUP" constant).
+             * @param timeLongPress Time in milliseconds for long press.
+             * @param ptrActionShort Pointer to a procedure for the short press. The procedure must be without parameters.
+             * @param ptrActionLong Pointer to a procedure for the long press. The procedure must be without parameters.
              */
-            void setPin(unsigned short pin);
+            Button(uint8_t pin, input mode, uint32_t timeLongPress, ptrProcedure ptrActionShort, ptrProcedure ptrActionLong);
 
             /**
              * This method sets the time for the long press.
              * @param timeLongPress Time in milliseconds for long press.
              */
-            void setTimeLongPress(unsigned long timeLongPress);
+            void setTimeLongPress(uint32_t timeLongPress);
 
             /**
              * This method sets the pointer to the procedure, for the short press.
@@ -89,66 +113,97 @@
             void setPtrActionLong(ptrProcedure ptrActionLong);
 
             /**
-             * This method gets the pin button.
-             * @return Digital pin of the button.
-             */             
-            unsigned short getPin();
-
-            /**
              * This method gets the time for the long press.
              * @return Time in milliseconds for long press.
              */          
-            unsigned long getTimeLongPress();
+            uint32_t getTimeLongPress();
 
             /**
              * This method gets the actual press, both for short and long. For short press there is the debouncing.
              * @return Value -1 if the pressure is long; 0 if there is not any pressure; 1 if the pressure is short.
              */     
-            unsigned short checkPress();
+            uint8_t checkPress();
 
         private:
-            unsigned short pin;                             // Pin of the button to read the status.
-            unsigned long timeLongPress;                    // Time in milliseconds for the long press.
-            ptrProcedure ptrActionLong;                     // Pointer to a procedure for short press.
-            ptrProcedure ptrActionShort;                    // Pointer to a procedure for long press.
-            unsigned long timeOut;                          // The end of time, calculated with the actual time plus "timeLongPress".
-            boolean pressed;                                // A flag to define if the button was already pressed.
-            short valuePress;                               // The return value of method "checkPress".
+            uint8_t pin;                                // Pin of the button to read the status.
+            uint8_t mode;                               // Mode of the input, between "INPUT" (with "NO_PULLUP" constant) and "INPUT_PULLUP" (with "PULLUP" constant).
+            uint32_t timeLongPress;                     // Time in milliseconds for the long press.
+            ptrProcedure ptrActionLong;                 // Pointer to a procedure for short press.
+            ptrProcedure ptrActionShort;                // Pointer to a procedure for long press.
+            uint8_t valuePress;                         // This variable will contain the value where the button will be considered pressed.
+            uint32_t timeOut;                           // The end of time, calculated with the actual time plus "timeLongPress".
+            boolean status;                             // A flag to define if the button was already pressed.
+            int8_t actualValue;                         // Actual value of the button.
+
+            /**
+             * This method sets the pin button.
+             * @param pin Digital pin of the button.
+             */
+            void setPin(uint8_t pin);
+
+            /**
+             * This method sets the input mode.
+             * @param mode Mode of the input.
+             */
+            void setMode(input mode);
+
+            /**
+             * This method sets the value where the button will be considered pressed.
+             */
+            void setValuePress();
 
             /** 
              * This method sets the timeout, that is the time within which the pressure will be considerated short.
              * @param timeOut Time in milliseconds.
              */
-            void setTimeOut(unsigned long timeOut);
+            void setTimeOut(uint32_t timeOut);
 
             /** 
              * This method sets the status of the button, if is pressed or not.
-             * @param pressed Boolean value "true" if the button has been pressed; "false" if not.
+             * @param status Boolean value "true" if the button has been pressed; "false" if not.
              */            
-            void setPressed(boolean pressed);
+            void setStatus(boolean status);
 
             /**
              * This method sets a symbolic value of the press in actual time, both for short and long.
-             * @param valuePress Symbolic value of press.
+             * @param actualValue Symbolic value of press.
              */
-            void setValuePress(short valuePress);
+            void setActualValue(int8_t actualValue);
+
+            /**
+             * This method gets the pin button.
+             * @return Digital pin of the button.
+             */             
+            uint8_t getPin();
+
+            /**
+             * This method gets the mode of the button.
+             * @return Mode of the button.
+             */             
+            uint8_t getMode();
+
+            /**
+             * This method gets the value where the button will be considered pressed.
+             * @return Value of the press.
+             */
+            uint8_t getValuePress();
 
             /**
              * This method gets the timeout, that is the end of time within which the press will be considerated short.
              * @return Time in milliseconds.
              */                
-            unsigned long getTimeOut();     
+            uint32_t getTimeOut();     
 
             /**
              * This method gets the status of the button, if is pressed or not.
              * @return Boolean value "true" if the button has been pressed; "false" if not.
              */          
-            boolean getPressed();           
+            boolean getStatus();           
 
             /**
              * This method gets a symbolic value of the press in actual time, both for short and long.
              * @return Symbolic value of press.
              */                  
-            short getValuePress();
+            int8_t getActualValue();
     };
 #endif
